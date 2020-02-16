e9a8b3e85a2f307ae539f61791853715d720ccc1
Manas Singh <39790333+kingmanas@users.noreply.github.com>
2020-02-16 19:31:35 +0530

    Changed quick.js
    The last version was incorrect

--- Sorting/quicksort/quick.js
+++ Sorting/quicksort/quick.js
@@ -1,6 +1,8 @@
 
 var angle = [];
-var d = 0, k;
+var d = 0, k, l, r;    
+var stack[]; 
+
 function Drawn_Arc(begin_angle, end_angle, arc_colour) {
     this.begin_angle = begin_angle;
     this.end_angle = end_angle;
@@ -14,21 +16,34 @@ function setup() {
     for (let i = 1; i <= 1000; i++) {
         angle.push(new Drawn_Arc(((i - 1) * PI) / 500, (i * PI) / 500, random(0, 2000)));
     }
-    // angle = angle.sort((a, b) => {
-    //     return a.arc_colour - b.arc_colour;
-    // });
+    l = 0, r = angle.length() - 1;
+    stack.push(l), stack.push(r);
 }
-function draw() {
-    frameRate(60);
-    for (var j = 0; j < angle.length - 1; j++) {
-        if (angle[j + 1].arc_colour > angle[j].arc_colour) {
-            swap(j);
-        }
-    }
 
-    if (d < angle.length) {
-        d++;
-    } else { noLoop() }
+function partition(l, r) {
+    let j = l - 1, x = angle[r].arc_colour;
+    for (let i = l; i < r; i++)
+        if (angle[i].arc_colour <= x)
+            swap(++j, i);
+    swap(r, j + 1);
+    return j + 1;
+}
+
+function draw() {
+    frameRate(60);    
+    if(stack.length == 0) noLoop();
+
+    r = stack.pop(), l = stack.pop(); 
+    let p = partition(arr, l, r); 
+    if (p - 1 > l) { 
+        stack.push(l); 
+        stack.push(p - 1); 
+    } 
+    if (p + 1 < h) { 
+        stack.push(p + 1); 
+        stack.push(h); 
+    } 
+    
     for (let i = 1; i <= angle.length; i++) {
         noStroke();
         fill(angle[i - 1].arc_colour, 2000, 1000);
@@ -36,9 +51,9 @@ function draw() {
     }
 }
 
-function swap(j) {
-    angle[j + 1].arc_colour += angle[j].arc_colour;
-    angle[j].arc_colour = angle[j + 1].arc_colour - angle[j].arc_colour;
-    angle[j + 1].arc_colour = angle[j + 1].arc_colour - angle[j].arc_colour;
+function swap(j, k) {
+    let temp = angle[k].arc_colour;
+    angle[k].arc_colour = angle[j].arc_colour;
+    angle[j].arc_colour = temp;
 }
 

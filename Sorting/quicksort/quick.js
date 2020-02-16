3072df7f2c7fbec43095548cad53a11dae7e54fb
Manas Singh <39790333+kingmanas@users.noreply.github.com>
2020-02-16 19:54:38 +0530

    Final correct version

--- Sorting/quicksort/quick.js
+++ Sorting/quicksort/quick.js
@@ -13,10 +13,10 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
     colorMode(HSL, 2000);
-    for (let i = 1; i <= 1000; i++) {
-        angle.push(new Drawn_Arc(((i - 1) * PI) / 500, (i * PI) / 500, random(0, 2000)));
+    for (let i = 1; i <= 500; i++) {
+        angle.push(new Drawn_Arc(((i - 1) * PI) / 250, (i * PI) / 250, random(0, 2000)));
     }
-    l = 0, r = angle.length() - 1;
+    l = 0, r = angle.length - 1;
     stack.push(l), stack.push(r);
 }
 
@@ -30,26 +30,27 @@ function partition(l, r) {
 }
 
 function draw() {
-    frameRate(40);    
+    frameRate(30);    
     if(stack.length == 0) noLoop();
 
     r = stack.pop(), l = stack.pop(); 
     console.log(r + " " + l);
-    let p = partition(arr, l, r); 
+    let p = partition(l, r); 
     if (p - 1 > l) { 
         stack.push(l); 
         stack.push(p - 1); 
     } 
-    if (p + 1 < h) { 
+    if (p + 1 < r) { 
         stack.push(p + 1); 
-        stack.push(h); 
+        stack.push(r); 
     } 
     
-    for (let i = 1; i <= angle.length; i++) {
+    for (let i = l; i <= r; i++) {
         noStroke();
-        fill(angle[i - 1].arc_colour, 2000, 1000);
-        arc(windowWidth / 2, windowHeight / 2, windowHeight * 4, windowHeight * 4, angle[i - 1].begin_angle, angle[i - 1].end_angle, PIE);
+        fill(angle[i].arc_colour, 2000, 1000);
+        arc(windowWidth / 2, windowHeight / 2, windowHeight * 4, windowHeight * 4, angle[i].begin_angle, angle[i].end_angle, PIE);
     }
+
 }
 
 function swap(j, k) {
@@ -57,4 +58,3 @@ function swap(j, k) {
     angle[k].arc_colour = angle[j].arc_colour;
     angle[j].arc_colour = temp;
 }
-
